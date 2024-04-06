62cef861421a2f68b8ba0df550604cc10e93a455
caiomelotec <caio.melotec@icloud.com>
2024-04-07 00:22:30 +0200

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { Navbar } from "./components/NavBar";
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
