3656e26a0f76223c8e3a04901012241c2bcb015c
caiomelotec <caio.melotec@icloud.com>
2024-05-08 22:35:41 +0200

    Register Validation Schema

--- src/util/schema.ts
+++ src/util/schema.ts
@@ -1,6 +1,33 @@
-import z from 'zod';
+import z from "zod";
 
 export const LoginFormSchema = z.object({
-  email: z.string().min(1, { message: 'Email is required' }).email({ message: 'Email is invalid' }),
-  password: z.string().min(6, { message: 'Password must be at least 6 characters long' })
-})
+  email: z
+    .string()
+    .min(1, { message: "Email is required" })
+    .email({ message: "Email is invalid" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long" }),
+});
+
+export const RegisterFormSchema = z
+  .object({
+    username: z.string().min(1, { message: "Username is required" }),
+    email: z
+      .string()
+      .min(1, { message: "Email is required" })
+      .email({ message: "Email is invalid" }),
+    password: z
+      .string()
+      .min(6, { message: "Password must be at least 6 characters long" })
+      .max(12, { message: "Password must be at most 12 characters long" }),
+    confirmPassword: z
+      .string()
+      .min(6, { message: "Password must be at least 6 characters long" })
+      .max(12, { message: "Password must be at most 12 characters long" }),
+  })
+  .required()
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Passwords do not match",
+  });
