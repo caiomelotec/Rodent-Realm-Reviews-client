2f1bca1bcdaf9a4b9800bff9cc5b5b36059435e0
caiomelotec <caio.melotec@icloud.com>
2024-05-08 20:28:30 +0200

    Login validation schema

---
+++ src/util/schema.ts
@@ -0,0 +1,6 @@
+import z from 'zod';
+
+export const LoginFormSchema = z.object({
+  email: z.string().min(1, { message: 'Email is required' }).email({ message: 'Email is invalid' }),
+  password: z.string().min(6, { message: 'Password must be at least 6 characters long' })
+})
